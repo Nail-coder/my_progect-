c7a6902a680503ec7b7808f8c222fd57ea5c246f
nail minigalin <minigalin.nail.21@gmail.com>
2024-08-08 21:15:44 +0500

    четвертое изменение

--- src/macks.py
+++ src/macks.py
@@ -1,8 +1,8 @@
 def get_mask_card_number(card_num: str) -> str | None:
     # Функцию маскировки номера банковской карты
     if card_num.isdigit() and len(card_num) == 16:
-        return (f'{card_num[4:]}{card_num[4:6]}'
-                f'{"*" * 2}{"*" * 4}{card_num[12:]}')
+        return (f'{card_num[:4]} {card_num[4:6]}'
+                f'{"*" * 2} {"*" * 4} {card_num[12:]}')
     else:
         return None
 
@@ -10,8 +10,10 @@ def get_mask_card_number(card_num: str) -> str | None:
 def get_mask_account(account_number: str) -> str | None:
     # Функцию маскировки номера банковского счета
     if account_number.isdigit() and len(account_number) == 20:
-        return f'{"*" * 2} {account_number[-4::]}'
+        return f'{"*" * 2}{account_number[-4::]}'
     else:
         return None
 
+
 print(get_mask_card_number("7000792289606361"))
+print(get_mask_account("12345678910124567892"))
--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,30 @@
-from src import macks
+from datetime import datetime
+
+from src.macks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(nums: str) -> str:
-    if 'счет' in nums:
-        return macks.get_mask_account(nums)
+    nums_list = nums.split()
+    if nums_list[0] == 'Счет':
+        account = get_mask_account(nums[-20:])
+        new_account = nums.replace(nums[-20:], account)
+        return new_account
     else:
-        cards = macks.get_mask_card_number(nums[-16:])
+        cards = get_mask_card_number(nums[-16:])
         new_card = nums.replace(nums[-16:], cards)
         return new_card
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
 
-def get_data(data:str) -> str:
-    return f'{data[8:10], {data[5:7]}, {data[0:4]}}'
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 12345678901234567892"))
+
+
+def get_data(data: str) -> str:
+    if data == "":
+        return ""
+
+    d = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
+    return d.strftime("%d.%m.%Y")
+
 
 print(get_data("2024-03-11T02:26:18.671407"))
